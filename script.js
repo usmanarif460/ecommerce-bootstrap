b02ccc0f67c8bc26a224a4b765cbd3ac13d501a1
usmanarif460 <usmanarif460@gmail.com>
2023-01-28 13:30:43 +0500

    updated page design

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-var grid = ('.collection-list').isotope({
-  // options
-});
-// filter items on button click
-('.filter-button-group').on( 'click', 'button', function() {
-  var filterValue = (this).attr('data-filter');
-  grid.isotope({ filter: filterValue });
-});
+const products = [
+  {
+    image: `/images/P1.jpg`,
+    title: "ANYLUV Blue Light Blocking Glasses Men",
+    description:
+      "Gaming Blue Screen Glasses Mens Blue Light Blocker Glasses Rectangle Metal frame UV400 Clear Lens",
+    checkout:
+      "https://www.amazon.com/ANYLUV-Light-Blocking-Glasses-Women/dp/B0B71H528L?crid=21MEGPRSLBS7P&keywords=anti+bluelight+glasses+men&qid=1674885065&sprefix=anti+bluel,aps,87&sr=8-3&th=1&linkCode=sl1&linkId=c92c8f2b591c7afae4f125ec4e4574d7&language=en_US",
+    button: "Checkout",
+  },
+
+  {
+    image: `/images/P2.jpg`,
+    title: "Newppon 7 Pack Waterproof Cell Phone Pouch",
+    description:
+      "Gaming Blue Screen Glasses Mens Blue Light Blocker Glasses Rectangle Metal frame UV400 Clear Lens",
+    checkout:
+      "https://www.amazon.com/ANYLUV-Light-Blocking-Glasses-Women/dp/B0B71H528L?crid=21MEGPRSLBS7P&keywords=anti+bluelight+glasses+men&qid=1674885065&sprefix=anti+bluel,aps,87&sr=8-3&th=1&linkCode=sl1&linkId=c92c8f2b591c7afae4f125ec4e4574d7&language=en_US",
+    button: "Checkout",
+  },
+];
