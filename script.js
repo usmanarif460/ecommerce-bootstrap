d02c21b7711bd7ad339699661414feb43dc497d3
usmanarif460 <usmanarif460@gmail.com>
2023-02-10 22:33:52 +0500

    fixed button text

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const products = [
       "Gaming Blue Screen Glasses Mens Blue Light Blocker Glasses Rectangle Metal frame UV400 Clear Lens",
     checkout:
       "https://www.amazon.com/ANYLUV-Light-Blocking-Glasses-Women/dp/B0B71H528L?crid=21MEGPRSLBS7P&keywords=anti+bluelight+glasses+men&qid=1674885065&sprefix=anti+bluel,aps,87&sr=8-3&th=1&linkCode=sl1&linkId=c92c8f2b591c7afae4f125ec4e4574d7&language=en_US",
-    button: "Checkout",
+    button: "Details",
   },
 
   {
@@ -16,6 +16,6 @@ const products = [
       "Gaming Blue Screen Glasses Mens Blue Light Blocker Glasses Rectangle Metal frame UV400 Clear Lens",
     checkout:
       "https://www.amazon.com/ANYLUV-Light-Blocking-Glasses-Women/dp/B0B71H528L?crid=21MEGPRSLBS7P&keywords=anti+bluelight+glasses+men&qid=1674885065&sprefix=anti+bluel,aps,87&sr=8-3&th=1&linkCode=sl1&linkId=c92c8f2b591c7afae4f125ec4e4574d7&language=en_US",
-    button: "Checkout",
+    button: "Details",
   },
 ];
