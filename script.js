228eb068a013e5da39efa8ac198bbd6d6267f108
usmanarif460 <usmanarif460@gmail.com>
2023-02-27 21:34:22 +0500

    updated link

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ const products = [
     title: "Newppon 7 Pack Waterproof Cell Phone Pouch",
     description:
       "Gaming Blue Screen Glasses Mens Blue Light Blocker Glasses Rectangle Metal frame UV400 Clear Lens",
-    checkout:
-      "https://www.amazon.com/Newppon-Pack-Waterproof-Phone-Pouch/dp/B0B4JDZKCC?crid=38EQPPP5H2CE0&keywords=water+proof+phone+shower+phone+holder+iphone&qid=1674884258&sprefix=water+proof+phone+shower+phone+holder+iphone,aps,120&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExNElVMTlSOElERjlMJmVuY3J5cHRlZElkPUEwNzIwMzU5UTYzWEpIRlZTVVJOJmVuY3J5cHRlZEFkSWQ9QTA3NDQ2MjcxTEQxSFBPR04yTFJKJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1&linkCode=sl1&linkId=73f56ff59108cad098e2a4f025259a8f&language=en_US",
+    checkout: "https://amazon.to/3Z0pgsm",
     button: "Details",
   },
 ];
