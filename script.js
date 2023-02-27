9ba279d6196118ad7593b57c51777b757fee482e
usmanarif460 <usmanarif460@gmail.com>
2023-02-27 21:44:46 +0500

    fixed links

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const products = [
     title: "ANYLUV Blue Light Blocking Glasses Men",
     description:
       "Gaming Blue Screen Glasses Mens Blue Light Blocker Glasses Rectangle Metal frame UV400 Clear Lens",
-
     checkout: "https://amazon.to/3Z0pgsm",
     button: "Details",
   },
@@ -13,7 +12,7 @@ const products = [
     image: `/images/P2.jpg`,
     title: "Newppon 7 Pack Waterproof Cell Phone Pouch",
     description:
-      "Gaming Blue Screen Glasses Mens Blue Light Blocker Glasses Rectangle Metal frame UV400 Clear Lens",
+      "Universal Water Proof Dry Bag with Neck Lanyard - Underwater Clear Cell Holder Large Protector for iPhone Samsung Galaxy for Beach Pool Swimming Kayaking",
     checkout:
       "https://www.amazon.com/Newppon-Pack-Waterproof-Phone-Pouch/dp/B0B4JDZKCC?crid=38EQPPP5H2CE0&keywords=water+proof+phone+shower+phone+holder+iphone&qid=1674884258&sprefix=water+proof+phone+shower+phone+holder+iphone,aps,120&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExNElVMTlSOElERjlMJmVuY3J5cHRlZElkPUEwNzIwMzU5UTYzWEpIRlZTVVJOJmVuY3J5cHRlZEFkSWQ9QTA3NDQ2MjcxTEQxSFBPR04yTFJKJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1&linkCode=sl1&linkId=73f56ff59108cad098e2a4f025259a8f&language=en_US",
     button: "Details",
