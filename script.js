422f989dcc6ba2b25febb063f8113c776b753ead
usmanarif460 <usmanarif460@gmail.com>
2023-02-27 21:37:37 +0500

    updated links

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const products = [
     title: "ANYLUV Blue Light Blocking Glasses Men",
     description:
       "Gaming Blue Screen Glasses Mens Blue Light Blocker Glasses Rectangle Metal frame UV400 Clear Lens",
-    checkout:
-      "https://www.amazon.com/ANYLUV-Light-Blocking-Glasses-Women/dp/B0B71H528L?crid=21MEGPRSLBS7P&keywords=anti+bluelight+glasses+men&qid=1674885065&sprefix=anti+bluel,aps,87&sr=8-3&th=1&linkCode=sl1&linkId=c92c8f2b591c7afae4f125ec4e4574d7&language=en_US",
+
+    checkout: "https://amazon.to/3Z0pgsm",
     button: "Details",
   },
 
@@ -14,7 +14,8 @@ const products = [
     title: "Newppon 7 Pack Waterproof Cell Phone Pouch",
     description:
       "Gaming Blue Screen Glasses Mens Blue Light Blocker Glasses Rectangle Metal frame UV400 Clear Lens",
-    checkout: "https://amazon.to/3Z0pgsm",
+    checkout:
+      "https://www.amazon.com/Newppon-Pack-Waterproof-Phone-Pouch/dp/B0B4JDZKCC?crid=38EQPPP5H2CE0&keywords=water+proof+phone+shower+phone+holder+iphone&qid=1674884258&sprefix=water+proof+phone+shower+phone+holder+iphone,aps,120&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExNElVMTlSOElERjlMJmVuY3J5cHRlZElkPUEwNzIwMzU5UTYzWEpIRlZTVVJOJmVuY3J5cHRlZEFkSWQ9QTA3NDQ2MjcxTEQxSFBPR04yTFJKJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1&linkCode=sl1&linkId=73f56ff59108cad098e2a4f025259a8f&language=en_US",
     button: "Details",
   },
 ];
